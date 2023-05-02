0f3b391f6ff5a8ad60529f34ae673e5921198544
Juan Antonio <jamll.muro@gmail.com>
2023-05-02 22:10:18 +0200

    Add context to handle the app theme

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import Kanban from './Kanban';
 import './index.css';
+import { ThemeProvider } from './context/ThemeContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Kanban />
+      <ThemeProvider>
+        <Kanban />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
