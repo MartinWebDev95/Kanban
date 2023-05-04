c2ed3d996f7359818d47aa96a2544068c826b62a
Juan Antonio <jamll.muro@gmail.com>
2023-05-04 14:34:28 +0200

    Add a new loading state in DatabaseContext

--- src/context/DatabaseContext.jsx
+++ src/context/DatabaseContext.jsx
@@ -6,10 +6,18 @@ function DatabaseProvider({ children }) {
   const [boards, setBoards] = useState([]);
   const [selectedBoard, setSelectedBoard] = useState({});
   const [taskStatus, setTaskStatus] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const value = useMemo(() => ({
-    boards, setBoards, selectedBoard, setSelectedBoard, taskStatus, setTaskStatus,
-  }), [boards, selectedBoard, taskStatus]);
+    boards,
+    setBoards,
+    selectedBoard,
+    setSelectedBoard,
+    taskStatus,
+    setTaskStatus,
+    loading,
+    setLoading,
+  }), [boards, selectedBoard, taskStatus, loading]);
 
   return (
     <dbContext.Provider value={value}>
