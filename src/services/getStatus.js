28d4f45f9e1d62f46586066b914b5177fc1ceeac
Juan Antonio <jamll.muro@gmail.com>
2023-05-04 13:13:13 +0200

    Add a function that return all task status that belong to the selected board

---
+++ src/services/getStatus.js
@@ -0,0 +1,14 @@
+import supabase from '../../supabase';
+
+const getStatus = async (selectedBoard) => {
+  try {
+    // Get all status that belongs to the selected board
+    const { data } = await supabase.from('status').select().eq('boardId', selectedBoard.id);
+
+    return data;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+export default getStatus;
