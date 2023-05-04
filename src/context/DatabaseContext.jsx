8b04f13b39c86b08354732caf042ee88a44c9312
Juan Antonio <jamll.muro@gmail.com>
2023-05-04 13:15:57 +0200

    Add a new state in DatabaseContext for taskStatus

--- src/context/DatabaseContext.jsx
+++ src/context/DatabaseContext.jsx
@@ -5,10 +5,11 @@ const dbContext = createContext();
 function DatabaseProvider({ children }) {
   const [boards, setBoards] = useState([]);
   const [selectedBoard, setSelectedBoard] = useState({});
+  const [taskStatus, setTaskStatus] = useState([]);
 
   const value = useMemo(() => ({
-    boards, setBoards, selectedBoard, setSelectedBoard,
-  }), [boards, selectedBoard]);
+    boards, setBoards, selectedBoard, setSelectedBoard, taskStatus, setTaskStatus,
+  }), [boards, selectedBoard, taskStatus]);
 
   return (
     <dbContext.Provider value={value}>
