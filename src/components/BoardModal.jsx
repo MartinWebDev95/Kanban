d8d13d8ef2cf97cb6151740934bf15b378309649
Juan Antonio <jamll.muro@gmail.com>
2023-05-05 11:36:34 +0200

    Add a new BoardModal component to add a new board and it is task status

---
+++ src/components/BoardModal.jsx
@@ -0,0 +1,95 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+
+import { useState } from 'react';
+
+function BoardModal({ openBoardModal, setOpenBoardModal, updating = false }) {
+  const [nameBoard, setNameBoard] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleCloseBoardModal = (e) => {
+    if (e.target.ariaLabel === 'newBoard-modal') {
+      setOpenBoardModal(false);
+
+      setNameBoard('');
+    }
+  };
+
+  return (
+    openBoardModal && (
+      <div
+        className="grid place-items-center bg-black/50 absolute z-30 top-0 left-0 bottom-0 w-screen h-screen py-8"
+        aria-label="newBoard-modal"
+        onClick={handleCloseBoardModal}
+      >
+        <form
+          className={`bg-white dark:bg-slate-800 rounded-md w-4/5 ${updating ? 'h-fit' : 'h-full'} px-8 py-8 lg:w-2/5 flex flex-col gap-8 overflow-y-scroll scrollbar-hide`}
+          onSubmit={handleSubmit}
+        >
+          <h2 className="text-black dark:text-white font-semibold text-lg text-left">
+            {updating ? 'Edit Board' : 'Add New Board'}
+          </h2>
+
+          <label htmlFor="taskName" className="flex flex-col gap-2">
+            <span className="text-gray-500 dark:text-white text-sm font-semibold text-left">
+              Board Name
+            </span>
+
+            <input
+              type="text"
+              name="taskName"
+              id="taskName"
+              value={nameBoard}
+              placeholder="e.g. Product Launch"
+              className="dark:bg-slate-800 border-2 rounded-md py-2 px-2 border-gray-200 dark:border-gray-500 text-black dark:text-white text-sm"
+              onChange={(e) => setNameBoard(e.target.value)}
+            />
+          </label>
+
+          <label htmlFor="taskName" className="flex flex-col gap-2">
+            <span className="text-gray-500 dark:text-white text-sm font-semibold">
+              Board Columns
+            </span>
+
+            <div className="flex items-center gap-2">
+              <input
+                type="text"
+                name="inputColumn"
+                id="inputColumn"
+                value=""
+                placeholder="e.g. Todo"
+                className="dark:bg-slate-800 border-2 rounded-md py-2 px-2 border-gray-200 dark:border-gray-500 flex-1 placeholder:text-sm dark:text-white text-black font-normal"
+              />
+
+              <button
+                type="button"
+                className="w-fit"
+              >
+                <img src="/assets/icon-cross.svg" alt="Delete input" />
+              </button>
+            </div>
+          </label>
+
+          <button
+            type="button"
+            className="w-full rounded-full text-white bg-indigo-700 dark:text-indigo-700 dark:bg-white py-2 font-semibold lg:hover:bg-indigo-500 lg:hover:text-white transition-all duration-300 ease-in-out"
+          >
+            + Add New Column
+          </button>
+
+          <button
+            type="submit"
+            className="w-full rounded-full bg-indigo-700 text-white py-2 font-semibold lg:hover:bg-indigo-500 transition-all duration-300 ease-in-out"
+          >
+            {updating ? 'Save Changes' : 'Create Board'}
+          </button>
+        </form>
+      </div>
+    )
+  );
+}
+
+export default BoardModal;
--- src/components/ListOfBoards.jsx
+++ src/components/ListOfBoards.jsx
@@ -1,10 +1,12 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import useDatabaseContext from '../hooks/useDatabaseContext';
 import getBoards from '../services/getBoards';
 import Board from './Board';
+import BoardModal from './BoardModal';
 
 function ListOfBoards() {
   const { boards, setBoards, setSelectedBoard } = useDatabaseContext();
+  const [openAddBoardModal, setOpenAddBoardModal] = useState(false);
 
   useEffect(() => {
     getBoards()
@@ -17,27 +19,36 @@ function ListOfBoards() {
   }, []);
 
   return (
-    <ul className="mt-4 pr-4 font-semibold w-full">
-      <li>
-        <button
-          type="button"
-          className="flex items-center gap-4 pl-6 py-4 rounded-r-full w-full text-indigo-700 lg:hover:text-indigo-700 dark:lg:hover:bg-white lg:hover:bg-gray-100 transition-all duration-200 ease-in-out group"
-        >
-          <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg" className="fill-indigo-700 group-hover:fill-gray-500">
-            <path d="M0 2.889A2.889 2.889 0 0 1 2.889 0H13.11A2.889 2.889 0 0 1 16 2.889V13.11A2.888 2.888 0 0 1 13.111 16H2.89A2.889 2.889 0 0 1 0 13.111V2.89Zm1.333 5.555v4.667c0 .859.697 1.556 1.556 1.556h6.889V8.444H1.333Zm8.445-1.333V1.333h-6.89A1.556 1.556 0 0 0 1.334 2.89V7.11h8.445Zm4.889-1.333H11.11v4.444h3.556V5.778Zm0 5.778H11.11v3.11h2a1.556 1.556 0 0 0 1.556-1.555v-1.555Zm0-7.112V2.89a1.555 1.555 0 0 0-1.556-1.556h-2v3.111h3.556Z" />
-          </svg>
+    <>
+      <ul className="mt-4 pr-4 font-semibold w-full">
+        <li>
+          <button
+            type="button"
+            className="flex items-center gap-4 pl-6 py-4 rounded-r-full w-full text-indigo-700 lg:hover:text-indigo-700 dark:lg:hover:bg-white lg:hover:bg-gray-100 transition-all duration-200 ease-in-out group"
+            onClick={() => setOpenAddBoardModal(true)}
+          >
+            <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg" className="fill-indigo-700 group-hover:fill-gray-500">
+              <path d="M0 2.889A2.889 2.889 0 0 1 2.889 0H13.11A2.889 2.889 0 0 1 16 2.889V13.11A2.888 2.888 0 0 1 13.111 16H2.89A2.889 2.889 0 0 1 0 13.111V2.89Zm1.333 5.555v4.667c0 .859.697 1.556 1.556 1.556h6.889V8.444H1.333Zm8.445-1.333V1.333h-6.89A1.556 1.556 0 0 0 1.334 2.89V7.11h8.445Zm4.889-1.333H11.11v4.444h3.556V5.778Zm0 5.778H11.11v3.11h2a1.556 1.556 0 0 0 1.556-1.555v-1.555Zm0-7.112V2.89a1.555 1.555 0 0 0-1.556-1.556h-2v3.111h3.556Z" />
+            </svg>
 
-          <span>
-            + Create New Board
-          </span>
-        </button>
-      </li>
+            <span>
+              + Create New Board
+            </span>
+          </button>
+        </li>
 
-      {boards?.map((board) => (
-        <Board key={board.id} board={board} />
-      ))}
+        {boards?.map((board) => (
+          <Board key={board.id} board={board} />
+        ))}
 
-    </ul>
+      </ul>
+
+      <BoardModal
+        openBoardModal={openAddBoardModal}
+        setOpenBoardModal={setOpenAddBoardModal}
+      />
+    </>
+
   );
 }
 
