1df23e614b03513492923caa66d1da8f12f1ce43
Juan Antonio <jamll.muro@gmail.com>
2023-05-16 10:51:14 +0200

    Change the getStatus function to get the tasks status order by id

--- src/services/getStatus.js
+++ src/services/getStatus.js
@@ -3,7 +3,11 @@ import supabase from '../../supabase';
 const getStatus = async (selectedBoard) => {
   try {
     // Get all status that belongs to the selected board
-    const { data } = await supabase.from('status').select().eq('board_id', selectedBoard.id);
+    const { data } = await supabase
+      .from('status')
+      .select()
+      .eq('board_id', selectedBoard.id)
+      .order('id', { ascending: true });
 
     return data;
   } catch (error) {
