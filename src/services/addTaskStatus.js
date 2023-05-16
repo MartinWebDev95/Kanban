2f4aa5d68b555853c8482e85fde64b2add7ee4f7
Juan Antonio <jamll.muro@gmail.com>
2023-05-16 10:52:13 +0200

    Change the addTaskStatus function to return the new task status added

--- src/services/addTaskStatus.js
+++ src/services/addTaskStatus.js
@@ -3,9 +3,12 @@ import supabase from '../../supabase';
 const addTaskStatus = async ({ taskStatus }) => {
   // Add new tasks status in the database that belong to the new board
   try {
-    await supabase
+    const { data } = await supabase
       .from('status')
-      .insert(taskStatus);
+      .insert(taskStatus)
+      .select();
+
+    return data[0];
   } catch (error) {
     throw new Error(error.message);
   }
