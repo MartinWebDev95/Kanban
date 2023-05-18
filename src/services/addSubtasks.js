b168901af2edffe1bee07543fa580da57b301d91
Juan Antonio <jamll.muro@gmail.com>
2023-05-18 16:38:23 +0200

    Change the addSubtasks function to return the subtasks added

--- src/services/addSubtasks.js
+++ src/services/addSubtasks.js
@@ -3,10 +3,12 @@ import supabase from '../../supabase';
 const addSubtasks = async ({ subtasks }) => {
   // Add new subtasks in the database that belong to the new task
   try {
-    await supabase
+    const { data } = await supabase
       .from('subtasks')
       .insert(subtasks)
       .select();
+
+    return data[0];
   } catch (error) {
     throw new Error(error.message);
   }
