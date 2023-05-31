dbcffa052fc87db14ffeed1fd504048d7e188eaa
Juan Antonio <jamll.muro@gmail.com>
2023-05-31 10:42:26 +0200

    Change the ListOfSubtasks component to show the subtasks of the selected task only if subtasks exist

--- src/components/ListOfSubtasks.jsx
+++ src/components/ListOfSubtasks.jsx
@@ -25,37 +25,39 @@ function ListOfSubtasks({ subtasks, setSubtasks }) {
   };
 
   return (
-    <div>
-      <h3 className="text-gray-500 font-semibold text-xs lg:text-sm mb-4 tracking-widest">
-        {`Subtasks (${subtasks.filter((subtask) => subtask.done).length} of ${subtasks.length})`}
-      </h3>
+    subtasks.length !== 0 && (
+      <div>
+        <h3 className="text-gray-500 font-semibold text-xs lg:text-sm mb-4 tracking-widest">
+          {`Subtasks (${subtasks.filter((subtask) => subtask.done).length} of ${subtasks.length})`}
+        </h3>
 
-      <ul>
-        {subtasks.map((subtask) => (
-          <li key={subtask.id} className="mb-2">
-            <label
-              htmlFor={`subtask-${subtask.id}`}
-              className={`${subtask.done ? 'line-through dark:text-gray-500 text-gray-500' : 'dark:text-white text-black'} flex items-center gap-3 rounded-lg dark:bg-slate-900 bg-gray-100 p-3 text-xs lg:text-sm font-semibold hover:cursor-pointer hover:bg-gray-300 dark:hover:bg-indigo-900 transition-all duration-100 ease-in-out`}
-            >
-              <input
-                type="checkbox"
-                name={`subtask-${subtask.id}`}
-                id={`${subtask.id}`}
-                onClick={handleCheckedSubtask}
-                checked={subtask.done}
-                className="w-4 h-4"
-                readOnly
-              />
+        <ul>
+          {subtasks.map((subtask) => (
+            <li key={subtask.id} className="mb-2">
+              <label
+                htmlFor={`subtask-${subtask.id}`}
+                className={`${subtask.done ? 'line-through dark:text-gray-500 text-gray-500' : 'dark:text-white text-black'} flex items-center gap-3 rounded-lg dark:bg-slate-900 bg-gray-100 p-3 text-xs lg:text-sm font-semibold hover:cursor-pointer hover:bg-gray-300 dark:hover:bg-indigo-900 transition-all duration-100 ease-in-out`}
+              >
+                <input
+                  type="checkbox"
+                  name={`subtask-${subtask.id}`}
+                  id={`${subtask.id}`}
+                  onClick={handleCheckedSubtask}
+                  checked={subtask.done}
+                  className="w-4 h-4"
+                  readOnly
+                />
 
-              <p className="w-full">
-                {subtask.name}
-              </p>
+                <p className="w-full">
+                  {subtask.name}
+                </p>
 
-            </label>
-          </li>
-        ))}
-      </ul>
-    </div>
+              </label>
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
   );
 }
 
