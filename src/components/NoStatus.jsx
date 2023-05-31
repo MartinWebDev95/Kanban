54119bbe213d67e83dd96338ce4f3834fd256aa2
Juan Antonio <jamll.muro@gmail.com>
2023-05-31 09:56:29 +0200

    Add a new NoStatus component which is showed when a board don't have status

---
+++ src/components/NoStatus.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import BoardModal from './BoardModal';
+
+function NoStatus() {
+  const [openAddBoardModal, setOpenAddBoardModal] = useState(false);
+
+  return (
+    <>
+      <div className="w-full h-full grid place-items-center">
+        <div className="flex flex-col items-center gap-4">
+          <h2 className="text-lg lg:text-xl font-semibold text-gray-500 text-center">
+            This board is empty. Create a new column to get started.
+          </h2>
+
+          <button
+            type="button"
+            className="bg-indigo-700 lg:hover:bg-indigo-500 transition-all duration-300 ease-in-out rounded-full text-white font-semibold px-4 py-2 flex items-center gap-2 text-base lg:text-xl"
+            onClick={() => setOpenAddBoardModal(true)}
+          >
+
+            + Add New Column
+
+          </button>
+        </div>
+      </div>
+
+      <BoardModal
+        openBoardModal={openAddBoardModal}
+        setOpenBoardModal={setOpenAddBoardModal}
+        updating
+      />
+    </>
+  );
+}
+
+export default NoStatus;
