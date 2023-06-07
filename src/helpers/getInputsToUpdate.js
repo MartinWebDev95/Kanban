4807e3886dc19ccb460d5895a3e47cc3f1037130
Juan Antonio <jamll.muro@gmail.com>
2023-06-07 11:56:59 +0200

    Change the valueInput property of input to value

--- src/helpers/getInputsToUpdate.js
+++ src/helpers/getInputsToUpdate.js
@@ -1,6 +1,6 @@
 const getInputsToUpdate = ({ initialState, inputs }) => inputs.filter(
   (input) => !initialState.some(
-    (state) => (input.idInput === state.id) && (input.valueInput === state.name),
+    (state) => (input.idInput === state.id) && (input.value === state.name),
   ),
 );
 
