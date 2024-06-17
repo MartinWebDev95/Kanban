3577ef81088f053753d3b3af21a55c18b92a792a
Juan Antonio <jamll.muro@gmail.com>
2024-06-17 14:34:32 +0200

    Change the Task component to put the button element inside a list element

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -15,19 +15,21 @@ function Task({ task }) {
 
   return (
     <>
-      <button
-        type="button"
-        className="dark:bg-slate-800 bg-white p-4 rounded-lg flex flex-col gap-2 shadow-lg group w-full"
-        onClick={() => setOpenTaskModal(true)}
-      >
-        <h2 className="dark:text-white text-black font-semibold text-left group-hover:text-indigo-700 transition-all duration-150 ease-in-out">
-          {task.name}
-        </h2>
+      <li>
+        <button
+          type="button"
+          className="dark:bg-slate-800 bg-white p-4 rounded-lg flex flex-col gap-2 shadow-lg group w-full"
+          onClick={() => setOpenTaskModal(true)}
+        >
+          <h2 className="dark:text-white text-black font-semibold text-left group-hover:text-indigo-700 transition-all duration-150 ease-in-out">
+            {task.name}
+          </h2>
 
-        <p className="text-gray-500 font-semibold text-sm">
-          {`${subtasks.filter((subtask) => subtask.done).length} of ${subtasks.length} subtasks`}
-        </p>
-      </button>
+          <p className="text-gray-500 font-semibold text-sm">
+            {`${subtasks.filter((subtask) => subtask.done).length} of ${subtasks.length} subtasks`}
+          </p>
+        </button>
+      </li>
 
       <TaskModal
         openTaskModal={openTaskModal}
